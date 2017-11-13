431a6a4596d0d2f469119f8a3269ddeb98ac4ac4
Teddi Maull <teddimaull@gmail.com>
2017-11-13 16:51:59 -0700

    add mod exports playground

---
+++ Node/NodeFilesystem/modexports_playground/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var greetings = require('./greetings');
+var quotes = require('./randomquotes');
+var numbers = require('./randomNums');
+
+// console.log(greetings); //what will this log?
+// greetings.hello("Teddi"); //what will this log?
+// greetings.bye("Teddi"); //what will this log?
+// greetings.noComment(); //what will this log?
+//
+// console.log(quotes); //what will this log?
+// console.log(quotes.hamiltonQuote()); //what will this log?
+// console.log(quotes.burrQuote()); //what will this log?
+//
+// console.log(numbers); //what will this log?
+// console.log(numbers.oneDigit()); //what will this log?
+// console.log(numbers.fourDigit()); //what will this log?
