58a9b4f19ab8c7dfad8ebeb1c37c35eca0ec6592
Teddi Maull <teddimaull@gmail.com>
2018-06-11 15:21:35 -0600

    move fullstack theater files into a directory

--- Misc/fullstacktheatercode.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('form').submit((e) => {
-    e.preventDefault()
-    $.post(`http://localhost:3000`, $(this).serialize())
-      .then((result) => {
-        $('#result').html(result)
-      })
-  })
-})
