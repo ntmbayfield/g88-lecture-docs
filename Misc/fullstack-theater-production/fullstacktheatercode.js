6f3fe1a7406b12c20b69b63a419e99cbc1694d96
Teddi Maull <teddimaull@gmail.com>
2018-06-18 14:31:33 -0600

    fix problem with context of this

--- Misc/fullstack-theater-production/fullstacktheatercode.js
+++ Misc/fullstack-theater-production/fullstacktheatercode.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('form').submit((e) => {
+  $('form').submit(function(e){
     e.preventDefault()
     $.post(`http://localhost:3000`, $(this).serialize())
       .then((result) => {
